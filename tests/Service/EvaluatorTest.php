5e2c5eca3950fb21ba75946a9ab26297694dc1ea
Karin Morais <karin.andrade@dnc.group>
2022-06-06 20:37:03 -0300

    Implemented test for highest bids

--- tests/Service/EvaluatorTest.php
+++ tests/Service/EvaluatorTest.php
@@ -26,9 +26,6 @@ class EvaluatorTest extends TestCase
     $Auction->receivesBid(new Bid($user2, 2000));
     $Auction->receivesBid(new Bid($user1, 2500));
     
-    
-    
-
     $Evaluator = new Evaluator();
 
     $Evaluator->evaluates($Auction);
@@ -107,5 +104,29 @@ class EvaluatorTest extends TestCase
     $lowestBid = $Evaluator->getLowestBid();
 
     self::assertEquals(200, $lowestBid);
+  }
+  
+  public function testevaluatorMustFind3HighestBids(): void {
+    $auction = new Auction("New car");
+
+    $user1 = new User("user1");
+    $user2 = new User("user2");
+    $user3 = new User("user3");
+    $user4 = new User("user4");
+
+    $auction->receivesBid(new Bid($user1, 1500));
+    $auction->receivesBid(new Bid($user2, 3000));
+    $auction->receivesBid(new Bid($user3, 500));
+    $auction->receivesBid(new Bid($user4, 1000));
+
+    $evaluator = new Evaluator();
+
+    $evaluator->evaluates($auction);
+
+    $highestBids = $evaluator->getHighestBids();
+    self::assertCount(3, $highestBids);
+    self::assertEquals(3000, $highestBids[0]->getValue());
+    self::assertEquals(1500, $highestBids[1]->getValue());
+    self::assertEquals(1000, $highestBids[2]->getValue());
   }
 }
